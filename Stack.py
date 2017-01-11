72ee4a7ae6f2d17aaeb8761072a9541dca09d552
Michael Palarya <palarya@gmail.com>
2017-01-11 12:14:25 +0200

    Added size and lock

--- Stack.py
+++ Stack.py
@@ -1,17 +1,36 @@
+from threading import Lock
 
-# TODO: limit max size, concurrent, more efficient size(), is_empty, clear
+# TODO: limit max size
+
+
 class Stack:
     def __init__(self):
         self.items = []
+        self.__size = 0
+        self.__lock = Lock()
 
     def push(self, item):
-        self.items.append(item)
+        with self.__lock:
+            self.__size += 1
+            self.items.append(item)
 
     def pop(self):
-        return self.items.pop()
+        with self.__lock:
+            if not self.is_empty():
+                self.__size -= 1
+                return self.items.pop()
 
     def peek(self):
         return self.items[self.size() - 1]
 
     def size(self):
-        return len(self.items)
+        return self.__size
+
+    def is_empty(self):
+        return len(self.items) > 0
+
+    def clear(self):
+        with self.__lock:
+            for i in range(self.size()):
+                self.pop()
+
