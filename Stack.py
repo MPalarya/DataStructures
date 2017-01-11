74ff52cf8d45106e6ee852c11a2dda8a80215cf2
Michael Palarya <palarya@gmail.com>
2017-01-11 13:23:14 +0200

    Added recursive reverse

--- Stack.py
+++ Stack.py
@@ -5,32 +5,53 @@ from threading import Lock
 
 class Stack:
     def __init__(self):
-        self.items = []
+        self.__items = []
         self.__size = 0
         self.__lock = Lock()
+
+    def __str__(self):
+        return "Stack: " + str(self.__items) + "< top"
 
     def push(self, item):
         with self.__lock:
             self.__size += 1
-            self.items.append(item)
+            self.__items.append(item)
 
     def pop(self):
         with self.__lock:
             if not self.is_empty():
                 self.__size -= 1
-                return self.items.pop()
+                return self.__items.pop()
 
     def peek(self):
-        return self.items[self.size() - 1]
+        return self.__items[self.size() - 1]
 
     def size(self):
         return self.__size
 
     def is_empty(self):
-        return len(self.items) > 0
+        return len(self.__items) == 0
 
     def clear(self):
         with self.__lock:
             for i in range(self.size()):
                 self.pop()
+
+    def reverse(self):
+        self.__items.reverse()
+
+    def reverse_recursive(self):
+        if self.is_empty():
+            return
+        item = self.pop()
+        self.reverse_recursive()
+        self.__recursive_insert_at_bottom(item)
+
+    def __recursive_insert_at_bottom(self, item):
+        if self.is_empty():
+            self.push(item)
+        else:
+            tmp = self.pop()
+            self.__recursive_insert_at_bottom(item)
+            self.push(tmp)
 
---
+++ StackChecker.py
@@ -0,0 +1,11 @@
+from Stack import Stack
+
+x = Stack()
+x.push(1)
+x.push(2)
+x.push(3)  # top
+print x
+
+x.reverse_recursive()
+print x
+
